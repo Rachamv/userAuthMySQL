af9b1fc6ccc9a949af9dee7cdc877c2229642a06
Adewunmi Oladele <87828937+Rachamv@users.noreply.github.com>
2022-10-28 12:25:32 +0100

    Update config.php

--- config.php
+++ config.php
@@ -3,10 +3,10 @@
 
 function db() {
     //set your configs here
-    $host = "localhost";
-    $user = "root";
-    $db = "zuriphp";
-    $password = "";
+    $host = '127.0.0.1';
+    $user = 'root';
+    $db = 'zuriphp';
+    $password = '';
     $conn = mysqli_connect($host, $user, $password, $db);
     if(!$conn){
         echo "<script> alert('Error connecting to the database') </script>";
